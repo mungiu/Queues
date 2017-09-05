abe945e08f7be150bbae1dabb59a4d0a05ffbf15
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-05 19:52:49 +0200

    Tutorial checkpoint

--- Queues/Program.cs
+++ Queues/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Queues
 {
@@ -6,7 +7,30 @@ namespace Queues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<string> tasks = new Queue<string>();
+
+            tasks.Enqueue("Do the washing up");
+            tasks.Enqueue("Finish C# module");
+            tasks.Enqueue("Buy chocolate");
+            tasks.Enqueue("Buy more chcolate");
+
+            Console.WriteLine("\r\nALLTASKS: ");
+            foreach (string title in tasks)
+                Console.WriteLine(title);
+
+            //removes and returns the object at the beginning of the queue
+            string nextTask = tasks.Dequeue();
+            Console.WriteLine("\r\n" + nextTask);
+            Console.WriteLine("\r\nALLTASKS: ");
+            foreach (string title in tasks)
+                Console.WriteLine(title);
+
+            //returns the object at the beginning of the queue without removing it
+            string nextTask2 = tasks.Peek();
+            Console.WriteLine("\r\n" + nextTask2);
+            Console.WriteLine("\r\nALLTASKS: ");
+            foreach (string title in tasks)
+                Console.WriteLine(title);
         }
     }
 }
